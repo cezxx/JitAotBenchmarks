c75e436fa57f61d95d4a7bb1304ef4b8539926e1
Cezary Kolaszewski <cezary.kolaszewski@utp.edu.pl>
2021-08-31 13:09:34 +0200

    Added load test endpoint

--- src/main/java/benchmark/ExampleResource.java
+++ src/main/java/benchmark/ExampleResource.java
@@ -13,4 +13,18 @@ public class ExampleResource {
     public String hello() {
         return "Hello Benchmarks!";
     }
+
+
+    @GET
+    @Path("/load")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String loadTest() {
+        int n = 30000;
+
+        Nbody.NBodySystem bodies = new Nbody.NBodySystem();
+        for (int i = 0; i < n; ++i) {
+            bodies.advance(0.01);
+        }
+        return "OK";
+    }
 }
